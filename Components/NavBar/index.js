49c58a004c1e07c1c70e764e92e561c0b2a0d9b1
Mell <mellkayrusson@gmail.com>
2020-04-24 18:35:43 +0100

    nav bar css

--- Components/NavBar/index.js
+++ Components/NavBar/index.js
@@ -1,22 +1,20 @@
 import React, { useState } from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
-import burgerMenu from "./burgermenu.svg"
-
+import burgerMenu from "./burgermenu.svg";
 
 function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-      
     <div className="navBar">
-    <p>LOGOHEREBAB</p>
+      <p>LOGOHEREBAB</p>
       <img
         src={burgerMenu}
         className="burgerMenu"
         alt="menu"
         onClick={() => setIsMenuOpen(!isMenuOpen)}
       />
-       <p className="header">AniMate</p>
+      <h3 className="header">AniMate</h3>
       {isMenuOpen && (
         <Menu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       )}
